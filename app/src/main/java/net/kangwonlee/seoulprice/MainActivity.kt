4f8a5601c420582d705d10745ee54d248ca24d42
kang-won Lee <lkwkang@gmail.com>
2021-06-27 03:38:48 +0900

    init project

---
+++ app/src/main/java/net/kangwonlee/seoulprice/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.kangwonlee.seoulprice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
